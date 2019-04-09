ddc4307218389059be770e6ea4a34aa4adce8a5f
kehuanhuan <1152018701@qq.com>
2019-04-09 11:41:53 +0800

    fix

--- include/functions.h
+++ include/functions.h
@@ -1,3 +1,9 @@
 extern char *get_local_ip();
 extern void change(int *a, int *b);
 extern char whoaim();
+
+struct Book {
+    char *title;
+    int pages;
+    char *author;
+};
--- main.c
+++ main.c
@@ -9,5 +9,18 @@ int main(void)
     change(&a, &b);
     printf("a now value is %d\n", a);
 
+    struct Book bk1;
+
+    bk1.title = "C Programming, by kehuanhuan";
+    bk1.pages = 10;
+
+    printBook(&bk1);
+
     return 0;
+}
+
+void printBook(struct Book *book)
+{
+    printf("Book title is %s\n", book->title);
+    printf("Book pages is %d\n", book->pages);
 }
